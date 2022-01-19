021815a4877987010bed848fe950479efebd6179
rnfkaus8 <kth4350@gmail.com>
2022-01-19 21:25:57 +0900

    refac : DI 설정

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,14 +1,23 @@
 package hello.core.member;
 
 import org.assertj.core.api.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import hello.core.AppConfig;
+
 public class MemberServiceTest {
+	MemberService memberService;
 	
-	MemberService memberService = new MemberServiceImpl();
+	@BeforeEach
+	public void beforeEach() {
+		AppConfig appConfig = new AppConfig();
+		memberService = appConfig.memberService();
+	}
 	
 	@Test
 	void join() {
+		
 		// given
 		Member member = new Member(1L, "memberA", Grade.VIP);
 		// when

c864e4c22d9fc4d32deabd01832755040e4b33cf
rnfkaus8 <kth4350@gmail.com>
2022-01-31 17:37:28 +0900

    의존관계 주입 : 수정자(setter 주입)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.FilterType;
 
 import static org.springframework.context.annotation.ComponentScan.*;
 @Configuration
-@ComponentScan(excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+@ComponentScan(
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
 public class AutoAppConfig {
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository(){

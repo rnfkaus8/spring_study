e03c52f54605c6f3365beedf5037d1ff5869757c
rnfkaus8 <kth4350@gmail.com>
2022-02-07 19:30:55 +0900

    빈 등록 초기화, 소멸 메소드 @Bean(initMethod = "초기화 메소드 명", destroyMethod = "소멸 메소드 명") 외부 라이브러리도 사용 가능 destroyMethod 는 추론 기능이 있어 close, shutdown 이라는 메소드를 자동으로 호출해줌

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,7 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient  {
     private String url;
 
     public NetworkClient() {
@@ -28,14 +26,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connent();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }

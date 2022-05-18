a7f03dfd7c1842991ccab917ee5ff179fda95138
rnfkaus8 <kth4350@gmail.com>
2022-05-18 18:50:34 +0900

    사용하지 않는 클래스 import 제거

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -2,15 +2,6 @@ package hello.servlet.web.frontcontroller.v5;
 
 import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v3.ControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
-import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
-import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
-import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
-import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
-import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 

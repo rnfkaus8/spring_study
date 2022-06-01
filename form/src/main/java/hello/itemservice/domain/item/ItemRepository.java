132a56a3ed731d8711ddbef0f33ba91a0475ce35
rnfkaus8 <kth4350@gmail.com>
2022-06-01 11:51:17 +0900

    타임리프 - 단일 체크박스  - 타임리프의 th:field 속성 사용 시 checkbox 의 경우 히든필드도 생성해 줌  - 값이 true 인 경우 checked="checked" 속성 추가해줌

--- form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -32,6 +32,10 @@ public class ItemRepository {
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
+        findItem.setItemType(updateParam.getItemType());
+        findItem.setOpen(updateParam.getOpen());
+        findItem.setRegions(updateParam.getRegions());
+        findItem.setDeliveryCode(updateParam.getDeliveryCode());
     }
 
     public void clearStore() {

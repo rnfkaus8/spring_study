92ec7c792040c47497b77a1e1c85e3c736873675
rnfkaus8 <kth4350@gmail.com>
2022-06-02 15:44:22 +0900

    웹 어플리케이션에 메시지 적용  - 타임리프 메시지 표현식 : #{...}  - 타임리프 메시지 표현식 파라미터 : #{...(파라미터)}

--- message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -3,10 +3,13 @@ package hello.itemservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Locale = " + Locale.getDefault());
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 

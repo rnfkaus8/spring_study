930da499ea269bbda7a6489de278f713417353bb
rnfkaus8 <kth4350@gmail.com>
2022-07-03 21:15:55 +0900

    JDBC 개발 - 수정, 삭제  - 등록과 비슷함, ResultSet.executeUpdate() 사용  - executeUpdate() : 쿼리를 실행하고 영향받은 row 수 반환

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,7 +19,7 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberV1", 10000);
+        Member member = new Member("memberV5", 10000);
         Member savedMember = repository.save(member);
 
         //findById
@@ -26,5 +27,14 @@ class MemberRepositoryV0Test {
         log.info("findMember={}", findMember);
         assertThat(findMember).isEqualTo(member);
 
+        //update: moneyL 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }

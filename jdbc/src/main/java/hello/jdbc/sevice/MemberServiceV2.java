4fe5d6e8485e5021fe1db18fb4803a6dd9fe77f8
rnfkaus8 <kth4350@gmail.com>
2022-07-05 16:23:28 +0900

    트랜잭션 - 적용2  - 커넥션 유지가 필요한 두 update 로직은 리포지토리에서 커넥션을 닫으면 안되고 서비스 로직이 끝날 때 트랜잭션을 종료하고 닫아야 함  - 애플리케이션에서 DB 트랜잭션을 적용하기 위해서 서비스 계층이 매우 지저분해지고, 복잡한 코드를 요구함  - 추가로 커넥션을 유지하도록 코드를 변경하는 것도 어려움

---
+++ jdbc/src/main/java/hello/jdbc/sevice/MemberServiceV2.java
@@ -0,0 +1,63 @@
+package hello.jdbc.sevice;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final MemberRepositoryV2 memberRepository;
+    private final DataSource dataSource;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection conn = dataSource.getConnection();
+        try {
+            conn.setAutoCommit(false); //트랜잭션 시작
+            //비즈니스 로직 실행
+            bizLogic(conn, fromId, toId, money);
+            conn.commit(); //성공 시 커밋
+        } catch (Exception e) {
+            conn.rollback(); //실패 시 롤백
+            throw new IllegalStateException(e);
+        } finally {
+            release(conn);
+        }
+    }
+
+    private void bizLogic(Connection conn, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(conn, fromId);
+        Member toMember = memberRepository.findById(conn, toId);
+
+        memberRepository.update(conn, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(conn, toId, toMember.getMoney() + money);
+    }
+
+    private void release(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.setAutoCommit(true); //커넥션 풀을 고려하여 autoCommit 을 true 로 변경
+                conn.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+
+}
